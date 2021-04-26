fab1d38a0cba25e5075ef39d16b0ec6d44243e84
Diep <Diep>
2021-04-26 21:09:30 +0800

    Init commit

---
+++ handler.test.ts
@@ -0,0 +1,6 @@
+describe('sample class 1', () => {
+ it('test case 1', () => console.log('abc'));
+ it('test case 2', () => console.log('abc'));
+ it('test case 3', () => console.log('abc'));
+ 
+});
